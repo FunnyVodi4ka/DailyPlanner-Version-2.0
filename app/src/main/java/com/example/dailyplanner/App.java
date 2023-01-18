864a7d38a7d6d2b44a14c2ef4c590a7fa2f17a37
FunnyVodi4ka <AlexZuevOfficial@yandex.ru>
2023-01-18 17:02:39 +0300

    Main class App has been update

--- app/src/main/java/com/example/dailyplanner/App.java
+++ app/src/main/java/com/example/dailyplanner/App.java
@@ -2,11 +2,49 @@ package com.example.dailyplanner;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
+import com.example.dailyplanner.data.ApplicationDatabase;
+import com.example.dailyplanner.data.TaskDao;
+
 public class App extends Application {
+
+    private ApplicationDatabase database;
+    private TaskDao noteDao;
+
+    private static App instance;
+
+    public static App getInstance() {
+        return instance;
+    }
 
     @Override
     public void onCreate(){
         super.onCreate();
 
+        instance = this;
+
+        database = Room.databaseBuilder(getApplicationContext(),
+                        ApplicationDatabase.class, "app-db-name")
+                .allowMainThreadQueries()
+                .build();
+
+        noteDao = database.taskDao();
+    }
+
+    public ApplicationDatabase getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(ApplicationDatabase database) {
+        this.database = database;
+    }
+
+    public TaskDao getTaskDao() {
+        return noteDao;
+    }
+
+    public void setTaskDao(TaskDao noteDao) {
+        this.noteDao = noteDao;
     }
 }
--- app/src/main/java/com/example/dailyplanner/data/ApplicationDatabase.java
+++ app/src/main/java/com/example/dailyplanner/data/ApplicationDatabase.java
@@ -1,10 +1,11 @@
 package com.example.dailyplanner.data;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.example.dailyplanner.model.Task;
 
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
-public  abstract  class ApplicationDatabase {
+public  abstract  class ApplicationDatabase extends RoomDatabase {
     public abstract TaskDao taskDao();
 }
